2dbf2f6e185093aad4720975872030c947a43cbd
Zech Heneveld <zechheneveld@Zechs-MacBook-Air.local>
2018-02-07 16:52:33 -0700

    delete Btn added for each entree

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
+var total = 0;
+
 $(document).ready(function () {
 
     init();
 });
 
-var total = 0;
 
 function init() {
 
     $("body").prepend("<p>Employee Salary Calculator</p>");
+
+
+    $("#employee").on('click', '.deleteBtn', function () {
+
+        console.log("Meow");
+
+    });
 }
 
 function submitEmployee() {
@@ -23,8 +31,14 @@ function submitEmployee() {
     $("#salary").val("");
 
 
-    $("#employee").append("<p>" + nameText + " : " + idText + " : $" + salaryText + "</p>");
+    //$("#employee").append("<p>" + nameText + " : " + idText + " : $" + salaryText + "</p>");
     $("#totalSalary").text("Total Salary: $" + total);
+
+    $("#employee").append("<div></div>");
+    var zechsNewElement = $("#employee").children().last();
+    zechsNewElement.append("<span>" + nameText + " : " + idText + " : $" + salaryText + "   " + "</span>");
+    zechsNewElement.append("<button class='deleteBtn'>Delete</button>");
+
 }
 
 function resetBtn() {
